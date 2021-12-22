91b4a48aefce73092de2ef767121c9bdce48c76a
Guptabhimanyu <96366301+Guptabhimanyu@users.noreply.github.com>
2021-12-22 13:27:01 +0530

    Add files via upload

---
+++ J005_Abhimanyu Gupta_Assignment-2/Array Java Code/PrintArray.java
@@ -0,0 +1,15 @@
+//Abhimanyu Gupta
+//J005
+//BTech Data Science
+// 70092000030
+
+//import java.until.Scanner
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println("Array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
